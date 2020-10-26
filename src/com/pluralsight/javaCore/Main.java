b73a97c933f183a81d34470eeee6a904db85da15
Ayush Singh <ayush.singh@lowes.com>
2020-10-26 13:48:37 +0530

    java-nio

--- src/com/pluralsight/javaCore/Main.java
+++ src/com/pluralsight/javaCore/Main.java
@@ -1,15 +1,18 @@
 package com.pluralsight.javaCore;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
-        doTryWithResources();
+        //bufferedReaderImpl();
+        javaNIOImpl();
     }
 
+    //try-with-resource
     public static void doTryWithResources(){
         char buff[] = new char[8];
         int length;
@@ -25,4 +28,51 @@ public class Main {
             System.out.println(e.getClass().getSimpleName());
         }
     }
+
+    //chaining streams
+    public static void chainingStreams(InputStream in){
+
+        int length;
+        char[] buff = new char[8];
+        try(InputStreamReader reader = new InputStreamReader(in)){
+            for(char charVal : buff){
+                System.out.println(charVal);
+            }
+
+        }
+        catch (IOException e){
+            System.out.println(e.getClass().getSimpleName());
+        }
+    }
+
+    //buffered-reader
+    public static void bufferedReaderImpl(){
+        String inputValue;
+        try(BufferedReader reader = new BufferedReader(new FileReader("file1.txt"))){
+            while((inputValue = reader.readLine())!= null){
+                System.out.println(inputValue);
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getClass().getSimpleName());
+        }
+    }
+
+    //java-nio
+    public static void javaNIOImpl(){
+        Path path = Paths.get("file1.txt");
+        String inputValue;
+        //instead of new BufferedReader, we use factory methods of Files
+        try(BufferedReader reader = Files.newBufferedReader(path)){
+            while((inputValue = reader.readLine())!= null){
+                System.out.println(inputValue);
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getClass().getSimpleName());
+        }
+
+    }
+
 }
